bca3a13c112da58970a23d50fb4b40792c20c3f6
girltalk <monkeymonkey@hanmail.net>
2020-08-04 23:30:49 +0900

    add

--- Pattern/Program.cs
+++ Pattern/Program.cs
@@ -6,8 +6,41 @@ namespace Pattern
     {
         static void Main(string[] args)
         {
-            DoStrategy();
+            //DoIterator();
+            //DoTemplateMethod();
+            //DoStrategy();
+            //DoObserver();
         }
+
+        static void DoIterator()
+        {
+            DinerMenu dinerMenu = new DinerMenu();
+            PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+
+            Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+            waitress.printMenu();
+        }
+
+        static void DoTemplateMethod()
+        {
+            CaffeinBeverage coffee = new Coffee();
+
+            coffee.prepareRecipe();
+        }
+
+        static void DoObserver()
+        {
+            WeatherData weatherData = new WeatherData();
+
+            CurrentconditionDisplay currentDisplay = new CurrentconditionDisplay(weatherData);
+            StatisticsDisplay stasticsDisplay = new StatisticsDisplay(weatherData);
+            ForcastDisplay forecastDisplay = new ForcastDisplay(weatherData);
+
+            weatherData.setMeasurements(80, 65, 30.4f);
+            weatherData.setMeasurements(75, 33, 58.9f);
+            weatherData.setMeasurements(31, 45, 34.7f);
+        }
+
 
         static void DoStrategy()
         {
